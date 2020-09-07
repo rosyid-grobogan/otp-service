d4dc8214d50d00203c048c0df718ba85cfa8df1d
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-07 21:17:36 +0700

    initializing project

---
+++ src/test/java/com/rg/microservices/otpservice/OtpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rg.microservices.otpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
