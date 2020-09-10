8d370d52373fa58a21aff1a9cdb1561a05f0856b
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-10 14:31:01 +0700

    load balancer

--- src/main/java/com/rg/microservices/otpservice/controller/OTPController.java
+++ src/main/java/com/rg/microservices/otpservice/controller/OTPController.java
@@ -4,7 +4,9 @@ import com.rg.microservices.otpservice.dto.RegisterCheckDto;
 import com.rg.microservices.otpservice.service.OTPService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OTPController {
     private final OTPService otpService;
+    private final Environment environment;
 
     @Autowired
-    public OTPController(OTPService otpService) {
+    public OTPController(OTPService otpService, Environment environment) {
         this.otpService = otpService;
+        this.environment = environment;
     }
 
 
@@ -26,5 +30,14 @@ public class OTPController {
         otpService.requestOTP(register);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/test-loadbalancer")
+    public String testLoadBalancer() {
+        String port = environment.getProperty("local.server.port");
+
+        log.debug("port: {}", port);
+
+        return "Ok with port: "+ port;
     }
 }
